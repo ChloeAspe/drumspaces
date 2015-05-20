a398a4ccc288975a7b5538e88809d6c0c3354ecb
ChloeAspe <chloeturquois@live.fr>
2015-05-20 13:44:30 +0200

    Index.js forgotten

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
+/// ----------- SAMPLEOBJECT CONSTRUCTION -----------------
 
-
-// SAMPLEOBJECT CONSTRUCTOR
+// constructor
 function sampleObject(argName, argX, argY, argType) {
-	this.sName = argName;
-	this.coordX = argX;
-	this.coordY = argY;
-	this.sType = argType;
+  this.sName = argName;
+  this.coordX = argX;
+  this.coordY = argY;
+  this.sType = argType;
   this.shortName = argName.replace(/.wav/, "").replace(/Kick|Snare|OpenHH|ClosedHH/, "");
 }
 
-//useless
-sampleObject.prototype.isHovered= false;
-sampleObject.prototype.isSelected= false;
-//global. useless
-sampleObject.selectedSample = '';
-
-var sampleObjects = {}
 function getSampleObjects(jsonData,type){
   var list = new Array();
     for ( var i in jsonData ){
@@ -25,15 +18,16 @@ function getSampleObjects(jsonData,type){
       type))
   }
   return list;
-
 }
 
-
+// constants
 var RADIUS = 6;
 var canvasDim = new Object();
 canvasDim.x = 250
 canvasDim.y =500
 
+// VIEWTYPE : changes between the 3 tasks, random order
+var viewType= 1; // 1-list, 2-spacerandom, 3-spaceorganized
 
 window.onload = function(){
   //get divs where we will draw
@@ -41,144 +35,65 @@ window.onload = function(){
   var snareCanvas = document.getElementById("snare-canvas");
   var ohhCanvas = document.getElementById("ohh-canvas");
   var chhCanvas = document.getElementById("chh-canvas");
-  var w = kickCanvas.width; // (all kick,snare,hh canvas have same dimensions-- as defined in .drumspace css)
-  var h = kickCanvas.height;
+  var h;
+  var viewScript = '';
 
-  // draw the sample spaces
-  var kickBonsai = bonsai.run(kickCanvas, {
-    url: 'bonsaicode.js',
-    height: canvasDim.x,
-    width: canvasDim.y,
-    sampleList: getSampleObjects(KickPos,"Kick"),
-    radius: RADIUS
+  //-------------- TASK TIMING ----------------
+
+  // User controls
+  // PAUSE TIMER-- in case of whatever problem during the experiment
+  $("#pause-btn").click(function() {
+    if(pause!=true) {  
+      pause = true;
+      console.log("timer pause");
+      alert("Click OK when ready");
+      pause = false;
+      console.log("timer resume");
+    } else {
+      alert("start?");
+      pause=false;
+      start=true;
+      $("#pause-btn > span").text("PAUSE ");
+      loadView();
+    }
   });
 
-  var snareBonsai = bonsai.run(snareCanvas, {
-    url: 'bonsaicode.js',
-    height: canvasDim.x,
-    width: canvasDim.y,
-    sampleList: getSampleObjects(SnarePos,"Snare"),
-    radius: RADIUS
+  // DONE WITH CURRENT TASK
+  $("#done-btn").click(function() {
+    var done= confirm("Sure?");
+    if(done==true){
+      //SAVE SET AND GO TO NEXT TASK
+      clearInterval(interval);
+      /*viewType=2;
+      start=false;
+      pause=true;
+      $("#pause-btn > span").text("START ");
+      time=0;
+      var interval=setInterval(count, 10000);*/
+    }
+    //else do nothing and continue current task
   });
 
-  var ohhBonsai = bonsai.run(ohhCanvas, {
-    url: 'bonsaicode.js',
-    height: canvasDim.x,
-    width: canvasDim.y,
-    sampleList: getSampleObjects(OpenHHPos,"OpenHH"),
-    radius: RADIUS
-  });
-
-  var chhBonsai = bonsai.run(chhCanvas, {
-    url: 'bonsaicode.js',
-    height: canvasDim.x,
-    width: canvasDim.y,
-    sampleList: getSampleObjects(ClosedHHPos,"ClosedHH"),
-    radius: RADIUS
-  });
-
-  // handle interface messages-- from the bonsaijs runner context
-  // the OSC msgs will be sent from here
-  kickBonsai.on('load', function() {
-    kickBonsai.on('message:rhythmPlay', function(data) {
-      console.log("rhythm play : "+ data.sName);
-      setMidiMap("Kick",data.sName);
-
-    }).on('message:soloPlay',function(data) {
-      playOne("Kick",data.sName); 
-    }).on('message:rhythmMute',function(data) {
-      setMuted("Kick",true);
-      // console.log("rhyth mute : "+ data.sName);
-    }).on('message:rhythmUnmute',function(data) {
-      setMuted("Kick",false);
-      // console.log("rhyth unmute : "+ data.sName);
-    // }).on('message:rhythmStop',function(data) {
-      // console.log("rhythm stop : "+ data.sName);
-    });
-  });
-
-  snareBonsai.on('load', function() {
-    snareBonsai.on('message:rhythmPlay', function(data) {
-      console.log("rhythm play : "+ data.sName);
-      setMidiMap("Snare",data.sName);
-    }).on('message:soloPlay',function(data) {
-      playOne("Snare",data.sName); 
-    }).on('message:rhythmMute',function(data) {
-      setMuted("Snare",true);
-      // console.log("rhyth mute : "+ data.sName);
-    }).on('message:rhythmUnmute',function(data) {
-      setMuted("Snare",false);
-      // console.log("rhyth unmute : "+ data.sName);
-    // }).on('message:rhythmStop',function(data) {
-      // console.log("rhythm stop : "+ data.sName);
-    });
-  });
-
-  ohhBonsai.on('load', function() {
-    ohhBonsai.on('message:rhythmPlay', function(data) {
-      setMidiMap("OpenHH",data.sName);
-      console.log("rhythm play : "+ data.sName);
-    }).on('message:soloPlay',function(data) {
-      playOne("OpenHH",data.sName); 
-    }).on('message:rhythmMute',function(data) {
-      setMuted("OpenHH",true);
-      // console.log("rhyth mute : "+ data.sName);
-    }).on('message:rhythmUnmute',function(data) {
-      setMuted("OpenHH",false);
-      // console.log("rhyth unmute : "+ data.sName);
-    // }).on('message:rhythmStop',function(data) {
-      // console.log("rhythm stop : "+ data.sName);
-    });
-  });
-
-  chhBonsai.on('load', function() {
-    chhBonsai.on('message:rhythmPlay', function(data) {
-      console.log("rhythm play : "+ data.sName);
-      setMidiMap("ClosedHH",data.sName);
-    }).on('message:soloPlay',function(data) {
-      playOne("ClosedHH",data.sName); 
-    }).on('message:rhythmMute',function(data) {
-      setMuted("ClosedHH",true);
-      // console.log("rhyth mute : "+ data.sName);
-    }).on('message:rhythmUnmute',function(data) {
-      setMuted("ClosedHH",false);
-      // console.log("rhyth unmute : "+ data.sName);
-    // }).on('message:rhythmStop',function(data) {
-      // console.log("rhythm stop : "+ data.sName);
-    });
-  });
-
-$("#PlayBtn").click(function() {
-  if($(this).find("p").text() == "PLAY RHYTHM") {
-    $(this).find("p").text("STOP RHYTHM");
-    StartMidi();
-  }else{
-    $(this).find("p").text("PLAY RHYTHM");
-    StopMidi();
-  }
-});
-
-
-//TASK TIMING
-
-  // TASK CHRONOMETER
+// TASK CHRONOMETER
   var time=0;
-  var pause = false;
+  var pause = true;
+  var start = false;
   var timeWidth=0;
   var timelineWidth = $("#timeline").css("width");
   var proportion = parseInt(timelineWidth)/300;
-  function updateTimeLine() {
-    if(time>=280) {
-      $("#timeline > span").css("background-color", "tomato");
+  
+  function updateTimeLine() { //updates chrono display
+    timeWidth = time *proportion;
+    $("#timeline > span").css("width", timeWidth);
+    if(time>=280) {  // change color at 20secs from chrono end
+      $("#timeline > span").css("background-color", "FF6347");
       if(time==300){
       //SAVE SET AND GO TO NEXT TASK
       }
     }
-    timeWidth = time *proportion;
-    $("#timeline > span").css("width", timeWidth);
-    console.log(timeWidth);
   }
-  function count() {
+
+  function count() { // increase Time by 10 every 10secs until 300secs
     if(pause!=true){
       if(time<300){
         time=time+10;
@@ -188,29 +103,170 @@ $("#PlayBtn").click(function() {
       }
     }
   }
-  var interval=setInterval(count, 10000);
 
-  //PAUSE TIMER-- in case of whatever problem during the experiment
-  $("#pause-btn").click(function() {
-    pause = true;
-    console.log("timer pause");
-    alert("Click OK when ready");
-    pause = false;
-    console.log("timer resume");
-  })
+  var interval=setInterval(count, 10000); //set chrono
 
-  //DONE
-  $("#done-btn").click(function() {
-    var done= confirm("Sure?");
-    if(done==true){
-      //SAVE SET AND GO TO NEXT TASK
+  //-------------- END TASK TIMING ----------------
+
+
+  // ------------- LOAD VIEW FCT ------------------
+  // (view is only loaded when user clicks START)
+  function loadView() {
+    switch(viewType) {
+      case 1: 
+        viewScript='bonsaicodeList.js';
+        h=14000;
+        $(".drumspace").addClass("listview");
+        $(".mute-btn").css("display", "inline");
+        console.log("listview");
+        break;
+      case 2:
+        viewScript='bonsaicode.js';
+        h=canvasDim.x;
+        $(".drumspace").removeClass("listview");
+        $(".mute-btn").css("display", "none");
+        console.log("spacerand view");
+        break;
+      case 3: 
+        viewScript='bonsaicode.js';
+        h=canvasDim.x;
+        console.log("spaceorg view");
+        break;
     }
-    //else do nothing and continue current task
-  })
 
+    console.log("LOADVIEW SWITCH SAYS "+viewScript);
 
-} // end window onload
+    // draw the sample spaces
+    window.kickBonsai = bonsai.run(kickCanvas, {
+      url: viewScript,
+      height: h,
+      width: canvasDim.y,
+      sampleList: getSampleObjects(KickPos,"Kick"),
+      radius: RADIUS
+    });
 
+    window.snareBonsai = bonsai.run(snareCanvas, {
+      url: viewScript,
+      height: h,
+      width: canvasDim.y,
+      sampleList: getSampleObjects(SnarePos,"Snare"),
+      radius: RADIUS
+    });
 
+    window.ohhBonsai = bonsai.run(ohhCanvas, {
+      url: viewScript,
+      height: h,
+      width: canvasDim.y,
+      sampleList: getSampleObjects(OpenHHPos,"OpenHH"),
+      radius: RADIUS
+    });
 
+    window.chhBonsai = bonsai.run(chhCanvas, {
+      url: viewScript,
+      height: h,
+      width: canvasDim.y,
+      sampleList: getSampleObjects(ClosedHHPos,"ClosedHH"),
+      radius: RADIUS
+    });
 
+    // handle interface messages-- from the bonsaijs runner context
+    // the OSC msgs will be sent from here
+    kickBonsai.on('load', function() {
+      kickBonsai.on('message:addToRhythm', function(data) {
+        console.log("add to rhythm : "+ data.sName);
+        setMidiMap("Kick",data.sName);
+
+      }).on('message:soloPlay',function(data) {
+        playOne("Kick",data.sName); 
+      }).on('message:rhythmMute',function(data) {
+        setMuted("Kick",true);
+        // console.log("rhyth mute : "+ data.sName);
+      }).on('message:rhythmUnmute',function(data) {
+        setMuted("Kick",false);
+        // console.log("rhyth unmute : "+ data.sName);
+      // }).on('message:rhythmStop',function(data) {
+        // console.log("rhythm stop : "+ data.sName);
+      });
+    });
+
+    snareBonsai.on('load', function() {
+      snareBonsai.on('message:addToRhythm', function(data) {
+        console.log("add to rhythm : "+ data.sName);
+        setMidiMap("Snare",data.sName);
+      }).on('message:soloPlay',function(data) {
+        playOne("Snare",data.sName); 
+      }).on('message:rhythmMute',function(data) {
+        setMuted("Snare",true);
+        // console.log("rhyth mute : "+ data.sName);
+      }).on('message:rhythmUnmute',function(data) {
+        setMuted("Snare",false);
+        // console.log("rhyth unmute : "+ data.sName);
+      // }).on('message:rhythmStop',function(data) {
+        // console.log("rhythm stop : "+ data.sName);
+      });
+    });
+
+    ohhBonsai.on('load', function() {
+      ohhBonsai.on('message:addToRhythm', function(data) {
+        setMidiMap("OpenHH",data.sName);
+        console.log("add to rhythm : "+ data.sName);
+      }).on('message:soloPlay',function(data) {
+        playOne("OpenHH",data.sName); 
+      }).on('message:rhythmMute',function(data) {
+        setMuted("OpenHH",true);
+        // console.log("rhyth mute : "+ data.sName);
+      }).on('message:rhythmUnmute',function(data) {
+        setMuted("OpenHH",false);
+        // console.log("rhyth unmute : "+ data.sName);
+      // }).on('message:rhythmStop',function(data) {
+        // console.log("rhythm stop : "+ data.sName);
+      });
+    });
+
+    chhBonsai.on('load', function() {
+      chhBonsai.on('message:addToRhythm', function(data) {
+        console.log("add to rhythm : "+ data.sName);
+        setMidiMap("ClosedHH",data.sName);
+      }).on('message:soloPlay',function(data) {
+        playOne("ClosedHH",data.sName); 
+      }).on('message:rhythmMute',function(data) {
+        setMuted("ClosedHH",true);
+        // console.log("rhyth mute : "+ data.sName);
+      }).on('message:rhythmUnmute',function(data) {
+        setMuted("ClosedHH",false);
+        // console.log("rhyth unmute : "+ data.sName);
+      // }).on('message:rhythmStop',function(data) {
+        // console.log("rhythm stop : "+ data.sName);
+      });
+    });
+
+    $("#PlayBtn").click(function() {
+      if($(this).find("p").text() == "PLAY RHYTHM") {
+        $(this).find("p").text("STOP RHYTHM");
+        StartMidi();
+      }else{
+        $(this).find("p").text("PLAY RHYTHM");
+        StopMidi();
+      }
+    });
+
+    var muteType;
+    $(".mute-btn").click(function() {
+      muteType = $(this).attr("alt");
+      if($(this).attr("src")=="img/mutebtn.svg") {
+        // UNMUTE KICK/SNARE/... in rhythm
+        console.log("unmute " + muteType);
+        $(this).attr("src", "img/unmute.svg");
+        window[muteType+'Bonsai'].sendMessage('mute', false);
+      } else {
+        // MUTE KICK/SNARE/... in rhythm
+        console.log("mute " + muteType);
+        $(this).attr("src", "img/mutebtn.svg");
+        window[muteType+'Bonsai'].sendMessage('mute', true);
+      }
+    });
+  };
+
+  // ------------- END LOADVIEW FCT ----------------
+
+} // end window.onload
